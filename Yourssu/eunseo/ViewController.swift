b16afc407c342aa2cb2def5b2dd572863551c7df
송은서 <song-eunseo@song-eunseoui-MacBookAir.local>
2024-04-30 22:33:30 +0900

    세팅

---
+++ Yourssu/eunseo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eunseo
+//
+//  Created by 송은서 on 4/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBOutlet weak var newButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+    @IBAction func addedButton(_ sender: Any) {
+    }
+}
+
---
+++ Yourssu/eunseo/ffViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ffViewController.swift
+//  eunseo
+//
+//  Created by 송은서 on 4/30/24.
+//
+
+import UIKit
+
+class ffViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
