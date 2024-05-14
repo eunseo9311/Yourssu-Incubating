d0b50f924014dd4172884499400920ef33da3119
eunseo9311 <songeunseo@soongsil.ac.kr>
2024-05-14 22:01:48 +0900

    1-assignment-calculator

--- Yourssu/eunseo/ViewController.swift
+++ Yourssu/eunseo/ViewController.swift
@@ -1,26 +1,69 @@
-//
-//  ViewController.swift
-//  eunseo
-//
-//  Created by 송은서 on 4/30/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var addButton: UIButton!
     
-    @IBOutlet weak var newButton: UIButton!
+    @IBOutlet weak var abstructButton: UIButton!
+    
+    @IBOutlet weak var multiplyButton: UIButton!
+    
+    @IBOutlet weak var divideButton: UIButton!
+    
+    @IBOutlet weak var inputFirst: UITextField!
+    
+    @IBOutlet weak var inputSecond: UITextField!
+    
+    @IBOutlet weak var info: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        
+        info.text = "버튼을 눌러주세요!"
     }
 
-
+    @IBAction func divideButton(_ sender: Any) {
+        guard let a = Double(inputFirst.text ?? ""), let b = Double(inputSecond.text ?? "") else {
+            info.text = "값을 먼저 입력해주세요"
+            return
+        }
+        
+        if b == 0 {
+            info.text = "0으로 나눌 수 없습니다"
+            return
+        }
+        
+        let result = a / b
+        info.text = "\(result)"
+    }
+    
+    @IBAction func multipeButton(_ sender: Any) {
+        guard let a = Double(inputFirst.text  ?? ""), let b = Double(inputSecond.text ?? "") else {
+            info.text = "값을 먼저 입력해주세요"
+            return
+        }
+        
+        let result = a * b
+        info.text = "\(result)"
+    }
+    
+    @IBAction func substractButton(_ sender: Any) {
+        guard let a = Double(inputFirst.text ?? ""), let b = Double(inputSecond.text ?? "") else {
+            info.text = "값을 먼저 입력해주세요"
+            return
+        }
+        
+        let result = a - b
+        info.text = "\(result)"
+    }
+    
     @IBAction func addedButton(_ sender: Any) {
-        print("test")
+        guard let a = Double(inputFirst.text ?? ""), let b = Double(inputSecond.text ?? "") else {
+            info.text = "값을 먼저 입력해주세요"
+            return
+        }
+        
+        let result = a + b
+        info.text = "\(result)"
     }
 }
 
