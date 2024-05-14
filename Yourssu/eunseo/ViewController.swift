83566543fcb9c87f0cf2386c36d919fab90aeffb
eunseo9311 <songeunseo@soongsil.ac.kr>
2024-05-14 22:16:02 +0900

    [refactor] 변수명 수정

--- Yourssu/eunseo/ViewController.swift
+++ Yourssu/eunseo/ViewController.swift
@@ -10,60 +10,60 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var divideButton: UIButton!
     
-    @IBOutlet weak var inputFirst: UITextField!
+    @IBOutlet weak var inputFirstTextField: UITextField!
     
-    @IBOutlet weak var inputSecond: UITextField!
+    @IBOutlet weak var inputSecondTextField: UITextField!
     
-    @IBOutlet weak var info: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        info.text = "버튼을 눌러주세요!"
+        infoLabel.text = "버튼을 눌러주세요!"
     }
 
     @IBAction func divideButton(_ sender: Any) {
-        guard let a = Double(inputFirst.text ?? ""), let b = Double(inputSecond.text ?? "") else {
-            info.text = "값을 먼저 입력해주세요"
+        guard let a = Double(inputFirstTextField.text ?? ""), let b = Double(inputSecondTextField.text ?? "") else {
+            infoLabel.text = "값을 먼저 입력해주세요"
             return
         }
         
         if b == 0 {
-            info.text = "0으로 나눌 수 없습니다"
+            infoLabel.text = "0으로 나눌 수 없습니다"
             return
         }
         
         let result = a / b
-        info.text = "\(result)"
+        infoLabel.text = "\(result)"
     }
     
     @IBAction func multipeButton(_ sender: Any) {
-        guard let a = Double(inputFirst.text  ?? ""), let b = Double(inputSecond.text ?? "") else {
-            info.text = "값을 먼저 입력해주세요"
+        guard let a = Double(inputFirstTextField.text  ?? ""), let b = Double(inputSecondTextField.text ?? "") else {
+            infoLabel.text = "값을 먼저 입력해주세요"
             return
         }
         
         let result = a * b
-        info.text = "\(result)"
+        infoLabel.text = "\(result)"
     }
     
     @IBAction func substractButton(_ sender: Any) {
-        guard let a = Double(inputFirst.text ?? ""), let b = Double(inputSecond.text ?? "") else {
-            info.text = "값을 먼저 입력해주세요"
+        guard let a = Double(inputFirstTextField.text ?? ""), let b = Double(inputSecondTextField.text ?? "") else {
+            infoLabel.text = "값을 먼저 입력해주세요"
             return
         }
         
         let result = a - b
-        info.text = "\(result)"
+        infoLabel.text = "\(result)"
     }
     
     @IBAction func addedButton(_ sender: Any) {
-        guard let a = Double(inputFirst.text ?? ""), let b = Double(inputSecond.text ?? "") else {
-            info.text = "값을 먼저 입력해주세요"
+        guard let a = Double(inputFirstTextField.text ?? ""), let b = Double(inputSecondTextField.text ?? "") else {
+            infoLabel.text = "값을 먼저 입력해주세요"
             return
         }
         
         let result = a + b
-        info.text = "\(result)"
+        infoLabel.text = "\(result)"
     }
 }
 
